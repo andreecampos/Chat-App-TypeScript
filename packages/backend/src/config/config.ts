3902a0f80c355b6cc18fd19514dd555873921515
andreecampos <orlandoandree1998@gmail.com>
2022-10-14 16:34:16 +0200

    login

--- packages/backend/src/config/config.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-export const PORT: number = parseInt(process.env.SERVER_PORT || '3001')
-export const MONGO_URL: string = process.env.MONGO_URL || 'mongodb+srv://chatapp:bZgTh4dZw8tnoLfh@cluster0.rjtccdb.mongodb.net/chatApp';
