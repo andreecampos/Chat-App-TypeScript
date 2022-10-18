11f498790f5f176b4ae1c44c988801fb3dda9b18
andreecampos <orlandoandree1998@gmail.com>
2022-10-18 16:10:39 +0200

    add space

--- packages/backend/src/models/user-repository.ts
+++ packages/backend/src/models/user-repository.ts
@@ -1,5 +1,5 @@
 import { connect, model, Schema } from "mongoose"
-import { UserItem }  from '@chat-app/shared'
+import { UserItem } from '@chat-app/shared'
 const bcrypt = require('bcrypt')
 
 export interface UserInfo {
@@ -7,31 +7,32 @@ export interface UserInfo {
     password: string,
     name: string,
     roles: string[],
- 
+
 }
 
 const UserInfoSchema = new Schema({
     username: String,
     password: String,
     roles: Array<String>,
-  
+
 })
 
-const UserInfoModel = model< UserInfo >('UserInfo', UserInfoSchema)
+const UserInfoModel = model<UserInfo>('UserInfo', UserInfoSchema)
 
-export const setupUser = async (url: string)=> {
+export const setupUser = async (url: string) => {
     await connect(url)
 }
 
 export const loadUserByUsername = async (username: string): Promise<UserInfo | null> => {
-    return await UserInfoModel.findOne({username: username}).exec()
+    return await UserInfoModel.findOne({ username: username }).exec()
 }
 
 // export const loadUserByUsername = async (): Promise<UserItem[]> => {
 //     return UserInfoModel.find({}).exec()
 // }
 
-export const saveUserItem = async (userItem: UserItem): Promise<void> =>{
+export const saveUserItem = async (userItem: UserItem): Promise<void> => {
+
     // const hash =  bcrypt.genSalt(10)
     // userItem.password = bcrypt.hash(userItem.password, hash)
     const newModel = new UserInfoModel(userItem)
